39377b27855c3f59d7248faffbcbf25de8bc134f
rudtnfl <50797976+rudtnfl@users.noreply.github.com>
2022-10-15 22:09:47 +0900

    Add files via upload

--- wallpad/run.sh
+++ wallpad/run.sh
@@ -1,30 +1,17 @@
 #!/bin/sh
 
 CONFIG_PATH=/data/options.json
-SHARE_DIR=/share
+SHARE_DIR=/share/bestin
 
-CUSTOM_FILE=$(jq --raw-output ".customfile" $CONFIG_PATH)
-MODEL=$(jq --raw-output ".model" $CONFIG_PATH)
-TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
-JS_FILE=$MODEL"_"$TYPE"_wallpad.js"
-
-if [ -f $SHARE_DIR/$CUSTOM_FILE ]; then
-	echo "[Info] Initializing with Custom file: "$CUSTOM_FILE
-	JS_FILE=$CUSTOM_FILE
-else
-  	if [ ! -f $SHARE_DIR/$JS_FILE ]; then
-		LS_RESULT=`ls $SHARE_DIR | grep wallpad`
-		if [ $? -eq 0 ]; then
-			rm $SHARE_DIR/*wallpad.js
-		fi
-        cp /js/$MODEL"_"$TYPE".js" $SHARE_DIR/$JS_FILE
-	fi
+if [ ! -f $SHARE_DIR/ipark.js ]; then
+	mkdir $SHARE_DIR
+	mv ipark.js $SHARE_DIR
 fi
 
 # start server
-echo "[Info] Wallpad Controller stand by... : "$JS_FILE
+echo "[Info] Bestin Wallpad with RS485 stand by... "
 
-JS_FILE=/$SHARE_DIR/$JS_FILE
-node $JS_FILE
+cd $SHARE_DIR
+node $SHARE_DIR/ipark.js
 
 #while true; do echo "still live"; sleep 1800; done
